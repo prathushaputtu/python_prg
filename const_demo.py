8d7dff04e5dff258e57c796a29ba53de2167c4e3
prathusha <prathushaputtu@gmail.com>
2022-12-13 09:04:37 +0530

    var and const demo practice

---
+++ const_demo.py
@@ -0,0 +1,6 @@
+PI = 3.14
+print(PI)
+
+PI = 2
+print(PI)
+print(type(PI))
---
+++ var_demo.py
@@ -0,0 +1,14 @@
+letters = [1, 2, 3, 4]
+str = ["apple", "ball", "cat" ]
+
+
+print(letters)
+print(type(letters))
+print(id(letters))
+
+print(letters[2])
+print(letters[-2])
+print(letters[2:])
+print(letters[2:3])
+
+print(str)
